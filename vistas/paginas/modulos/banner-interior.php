2b5c2921e14e6b73697ac925a8e4015fa0f071b7
Jerson Arnual Moreno Quevedo <morenojea@globalhitss.com>
2023-05-04 17:19:00 -0500

    se elimina  archivos sin uso

--- vistas/paginas/modulos/banner-interior.php
+++ vistas/paginas/modulos/banner-interior.php
@@ -3,11 +3,7 @@ BANNER-INTERIOR
 ======================================-->
 
 <div class="bannerInterior container-fluid bg-white p-0 d-none d-lg-block">
-	
 	<figure>
-		
 		<img src="img/banner01.jpg" class="img-fluid" width="100%">
-
 	</figure>
-
 </div>
